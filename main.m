6ce2603aed363ace206b3060b034ba57f16c3c05
Fahad Abdullah <70315737+Abdullahkhan5@users.noreply.github.com>
2022-03-09 12:30:38 +0500

    Added Source code file

---
+++ main.m
@@ -0,0 +1,20 @@
+%Reading the file and creating images frame by frame
+v = VideoReader('C:\Users\DELL\Documents\MATLAB\BabarAzam.mp4'); 
+
+for img = 1:v.NumberOfFrames;
+    filename=strcat('frame',num2str(img),'.jpg');
+    b = read(v, img);
+    imwrite(b,filename);
+end
+
+%Displaying the image one by one in forward order
+for i = 1:img;
+    forwardfilename=strcat('frame',num2str(i),'.jpg');
+    imshow(forwardfilename,'InitialMagnification',40); %I choose 40 because it worked perfectly for my video size you can choose what fits you
+end
+
+%Displaying the image one by one in reverse order
+for j = img:-1:1;
+    reversefilename=strcat('frame',num2str(j),'.jpg');
+    imshow(reversefilename,'InitialMagnification',40);
+end
